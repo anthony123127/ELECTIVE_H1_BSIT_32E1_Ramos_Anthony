00c983a39ebc8d466712a879a5abcdd9603a2892
Anthony Ramos <skyjump00123@gmail.com>
2026-01-12 02:35:29 +0800

    Add project files.

---
+++ ELECTIVE_H1_BSIT_32E1_Ramos_Anthony/Models/Resolution.cs
@@ -0,0 +1,11 @@
+﻿namespace ELECTIVE_H1_BSIT_32E1_Ramos_Anthony.Models
+{
+    public class Resolution
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public bool IsDone { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
---
+++ ELECTIVE_H1_BSIT_32E1_Ramos_Anthony/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddCors();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors(policy =>
+    policy.AllowAnyOrigin()
+          .AllowAnyMethod()
+          .AllowAnyHeader());
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
